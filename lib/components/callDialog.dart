d4f124b516f15d850cb14ccef7caa094b8ee6149
John Paul Kpantey <Jpbroadvision@users.noreply.github.com>
2021-02-27 19:10:43 +0000

    router for signature

---
+++ lib/components/callDialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:coe_attendance/components/signature.dart';
+
+class CallSignatureDialog extends StatefulWidget {
+  @override
+  _CallSignatureDialogState createState() => _CallSignatureDialogState();
+}
+
+class _CallSignatureDialogState extends State<CallSignatureDialog> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // final TextEditingController _textEditingController = TextEditingController();
+
+  Future<void> showInformationDialog(BuildContext context){
+      }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: FlatButton(
+              color: Colors.deepOrange,
+              onPressed: () async {
+                await showInformationDialog(context);
+              },
+              child: Text(
+                "Stateful Dialog",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )),
+        ),
+      ),
+    );
+  }
+}
