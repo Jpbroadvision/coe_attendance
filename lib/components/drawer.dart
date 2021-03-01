c21a82b66d4f6c2a1452bf7804f328e43c9db4d0
Okai Yeboah <kingkloy16@gmail.com>
2021-03-01 04:37:06 +0000

    add drawer and expansion card

---
+++ lib/components/drawer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:coe_attendance/main.dart';
+import 'package:coe_attendance/records.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => MyHomePage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.group),
+            title: Text("Records"),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => Records()));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/invigilator_card.dart
@@ -0,0 +1,109 @@
+import 'package:coe_attendance/models/inivigilators_details_model.dart';
+import 'package:flutter/material.dart';
+
+class InvigilatorCard extends StatelessWidget {
+  final InvigilatorsDetailsModel invigilatorsDetails;
+
+  InvigilatorCard({@required this.invigilatorsDetails});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 10.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      elevation: 2.0,
+      child: ExpansionTile(
+        backgroundColor: Colors.white,
+        childrenPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25),
+        title: _buildTitle(),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Session'),
+              Text(
+                '${invigilatorsDetails.session}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Start Time'),
+              Text(
+                '${invigilatorsDetails.startTime}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('End Time'),
+              Text(
+                '${invigilatorsDetails.endTime}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Room'),
+              Text(
+                '${invigilatorsDetails.room}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Date'),
+              Text(
+                '${invigilatorsDetails.dateTime}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // title with arrow icon for expanded card
+  Widget _buildTitle() {
+    return Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image:
+                DecorationImage(image: AssetImage("assets/user-profile.png")),
+          ),
+          height: 50,
+          width: 50,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              invigilatorsDetails.name,
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              '${invigilatorsDetails.day}',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
