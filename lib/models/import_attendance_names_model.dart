cce6af5b58fa3f39161647bb1b85a811bc4c3a5b
Jpbroadvision <paulkpantey@gmail.com>
2021-03-18 00:08:20 +0000

    model file for import of attendance names from excel

---
+++ lib/models/import_attendance_names_model.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+class AttNamesModel extends Equatable {
+  int id;
+  String attName;
+
+  AttNamesModel({this.id, this.attName});
+
+  // converts inivigilators details to a Map
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{'id': id, 'name': attName};
+
+    return map;
+  }
+
+  // destruct map of inivigilators
+  AttNamesModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    attName = map['name'];
+  }
+
+  @override
+  List<Object> get props => [id, attName];
+}
