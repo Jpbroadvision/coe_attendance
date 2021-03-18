4a9751c165d5fad0c40c529bb2aaee2091727a02
Jpbroadvision <paulkpantey@gmail.com>
2021-03-18 00:09:20 +0000

    model for import names of Tas

---
+++ lib/models/import_tas_names_model.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+class TaNamesModel extends Equatable {
+  int id;
+  String taName;
+  String taRoomAlloc;
+
+  TaNamesModel({this.id, this.taName, this.taRoomAlloc});
+
+  // converts inivigilators details to a Map
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'id': id,
+      'name': taName,
+      'taRoomAlloc': taRoomAlloc
+    };
+
+    return map;
+  }
+
+  // destruct map of inivigilators
+  TaNamesModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    taName = map['name'];
+    taRoomAlloc = map['taRoomAlloc'];
+  }
+
+  @override
+  List<Object> get props => [id, taName, taRoomAlloc];
+}
