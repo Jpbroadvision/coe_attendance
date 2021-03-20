cfc62616281df6e9f4ea85382021747a7ef5b144
Okai Yeboah <kingkloy16@gmail.com>
2021-03-20 11:59:10 +0000

    refactor permission service

--- lib/service/permission_service.dart
+++ lib/service/permission_service.dart
@@ -2,16 +2,14 @@ import 'package:permission_handler/permission_handler.dart';
 
 class PermissionService {
   static void getPermission() async {
-    await PermissionHandler()
-        .checkPermissionStatus(PermissionGroup.storage)
-        .then((status) {
-      if (status == PermissionStatus.denied) {
-        requestPermission();
-      }
-    });
+    var status = await Permission.storage.status;
+
+    if (status == PermissionStatus.denied) {
+      requestPermission();
+    }
   }
 
   static void requestPermission() async {
-    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    await Permission.storage.request();
   }
 }
