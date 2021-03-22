2961b2d359f0e8e35aad21ae5dd9ad46656c7687
Okai Yeboah <kingkloy16@gmail.com>
2021-03-22 02:24:37 +0000

    add service locator

---
+++ lib/locator.dart
@@ -0,0 +1,8 @@
+import 'package:coe_attendance/service/database_service.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => DatabaseService());
+}
