106579cbc99c4ba73077ef32f3542ff97d02434e
Jpbroadvision <paulkpantey@gmail.com>
2021-03-24 16:08:25 +0000

    removed image encoding from csv file export

--- lib/main.dart
+++ lib/main.dart
@@ -359,8 +359,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                         category: _selectedCategory,
                                         duration: _selectedDuration,
                                         room: _selectedRoom,
-                                        dateTime: dateTime,
-                                        signImagePath: signImagePath);
+                                        dateTime: dateTime,);
 
                                 // save details to database
                                 try {
