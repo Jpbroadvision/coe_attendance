f20c88fc75e870a16033e3b9ad673ae93bc424ad
Okai Yeboah <kingkloy16@gmail.com>
2021-05-09 01:36:46 +0100

    refactor and rename InvigilatorCard

--- lib/src/screens/components/proctor_card.dart
+++ lib/src/screens/components/proctor_card.dart
@@ -3,25 +3,25 @@ import 'dart:io';
 import 'package:coe_attendance/src/core/models/attendance_records_model.dart';
 import 'package:flutter/material.dart';
 
-class InvigilatorCard extends StatelessWidget {
-  final AttendanceRecordsModel attendanceRecord;
+class ProctorCard extends StatelessWidget {
+  final AttendanceRecordModel attendanceRecord;
   final Function deleteFunction;
 
-  InvigilatorCard({@required this.attendanceRecord, this.deleteFunction});
+  ProctorCard({@required this.attendanceRecord, this.deleteFunction});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.only(bottom: 10.0),
+      margin: EdgeInsets.only(bottom: 5.0),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
           Radius.circular(10.0),
         ),
       ),
-      elevation: 2.0,
+      elevation: 1.0,
       child: ExpansionTile(
         backgroundColor: Colors.white,
-        childrenPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25),
+        childrenPadding: EdgeInsets.fromLTRB(25, 0, 25, 10),
         title: _buildTitle(),
         children: [
           Row(
@@ -57,9 +57,9 @@ class InvigilatorCard extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Date/Time'),
+              Text('Date'),
               Text(
-                '${attendanceRecord.dateTime}',
+                '${attendanceRecord.date}',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               )
             ],
@@ -71,49 +71,35 @@ class InvigilatorCard extends StatelessWidget {
 
   // title with arrow icon for expanded card
   Widget _buildTitle() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5.0),
-              child: ClipOval(
-                child: Material(
-                  shadowColor: Colors.grey.withOpacity(0.5),
-                  child: Image.file(
-                    File(attendanceRecord.signImagePath),
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  attendanceRecord.name,
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                  style: TextStyle(fontSize: 12),
-                ),
-                Text(
-                  '${attendanceRecord.category}',
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-          ],
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+      leading: ClipOval(
+        child: Material(
+          shadowColor: Colors.grey.withOpacity(0.5),
+          child: Image.file(
+            File(attendanceRecord.signImagePath),
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.delete, color: Colors.red),
-          onPressed: () {
-            deleteFunction(attendanceRecord.id);
-          },
-        ),
-      ],
+      ),
+      title: Text(
+        attendanceRecord.name,
+        overflow: TextOverflow.clip,
+        maxLines: 1,
+        style: TextStyle(fontSize: 15),
+      ),
+      subtitle: Text(
+        '${attendanceRecord.category}',
+        style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.delete, color: Colors.red),
+        onPressed: () {
+          deleteFunction(attendanceRecord.id);
+        },
+      ),
     );
   }
 }
