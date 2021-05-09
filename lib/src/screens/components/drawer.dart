951461594b328e5e64635fbfa8d3b8dd96f4f7d0
Okai Yeboah <kingkloy16@gmail.com>
2021-05-09 01:34:44 +0100

    remove navigation to varoius screens

--- lib/src/screens/components/drawer.dart
+++ lib/src/screens/components/drawer.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../../locator.dart';
+import '../../../utils/date_time.dart';
 import '../../../utils/pdf_document.dart';
 import '../../core/service/database_service.dart';
-import '../views/import_attendance.dart';
-import '../views/records.dart';
-import '../views/home.dart';
 import 'toast_message.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -19,27 +17,25 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home"),
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => HomePage()));
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.group),
-            title: Text("Records"),
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => Records()));
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.close,
+                  color: Theme.of(context).primaryColor,
+                ),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
           ),
           ListTile(
             leading: Icon(Icons.file_download),
-            title: Text("Export Today's Entries"),
+            title: Text("Export Today's Record(PDF)"),
             onTap: () async {
-              String path = await _databaseService.generateCSVPerDay();
+              final dateTimeHelper = DateTimeHelper();
+              String path = await _databaseService
+                  .generateCSV(dateTimeHelper.formattedDate);
 
               String message = "Failed to export your daily entries";
 
@@ -53,7 +49,34 @@ class CustomDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(Icons.file_download),
-            title: Text("Export All to CSV"),
+            title: Text("Export Today's Record(CSV)"),
+            onTap: () async {
+              final dateTimeHelper = DateTimeHelper();
+              String path = await _databaseService
+                  .generateCSV(dateTimeHelper.formattedDate);
+
+              String message = "Failed to export your daily entries";
+
+              if (path == null)
+                toastMessage(scaffoldKey.currentContext, message, Colors.red);
+              else {
+                message = "File saved at $path";
+                toastMessage(scaffoldKey.currentContext, message);
+              }
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.file_download),
+            title: Text("Export All(PDF)"),
+            onTap: () async {
+              await generatePDF();
+              toastMessage(
+                  scaffoldKey.currentContext, "File saved successfully.");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.file_download),
+            title: Text("Export All(CSV)"),
             onTap: () async {
               String path =
                   await _databaseService.generateCSV(); //generateCSVPerDay
@@ -68,23 +91,6 @@ class CustomDrawer extends StatelessWidget {
               }
             },
           ),
-          ListTile(
-            leading: Icon(Icons.file_download),
-            title: Text("Export PDF"),
-            onTap: () async {
-              await generatePDF();
-              toastMessage(
-                  scaffoldKey.currentContext, "File saved successfully.");
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.file_download),
-            title: Text("Import from CSV"),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => ImportAttendance()));
-            },
-          )
         ],
       ),
     );
