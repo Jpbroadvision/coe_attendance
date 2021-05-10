df68abf27974e32c9ffa08ba2e44527281b5361d
Okai Yeboah <kingkloy16@gmail.com>
2021-05-10 18:16:18 +0100

    add customColor and BottomNavigationPage

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
-import 'screens/views/home.dart';
+import 'screens/views/bottom_navigation.dart';
 
 class MyApp extends StatelessWidget {
+  static Map<int, Color> color = {
+    50: Color(0xFFfdc029).withOpacity(.1),
+    100: Color(0xFFfdc029).withOpacity(.2),
+    200: Color(0xFFfdc029).withOpacity(.3),
+    300: Color(0xFFfdc029).withOpacity(.4),
+    400: Color(0xFFfdc029).withOpacity(.5),
+    500: Color(0xFFfdc029).withOpacity(.6),
+    600: Color(0xFFfdc029).withOpacity(.7),
+    700: Color(0xFFfdc029).withOpacity(.8),
+    800: Color(0xFFfdc029).withOpacity(.9),
+    900: Color(0xFFfdc029).withOpacity(1),
+  };
+
+  final MaterialColor customColor = MaterialColor(0xFFfdc029, color);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CoE INVIGILTORS ATTENDANCE',
       theme: ThemeData(
-        primaryColor: const Color(0xFFFFFFFF),
-        accentColor: const Color(0xFF9c27b0),
-        canvasColor: const Color(0xFFECEFF1),
+        primarySwatch: customColor,
       ),
-      home: HomePage(),
+      home: BottomNavigationPage(),
       debugShowCheckedModeBanner: false,
     );
   }
